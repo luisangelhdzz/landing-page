7b335267959c2ac564ba3b914e9e7534031c4cc1
luisAngel <hluisangel96@gmail.com>
2026-07-22 21:56:15 -0600

    cambios

---
+++ main.js
@@ -0,0 +1,5 @@
+const hamburguerMenu=document.querySelector('#hamburguer-menu');
+const navbar=document.getElementById('navbar');
+hamburguerMenu.addEventListener('click',()=>{
+    navbar.style.display ='block'
+})
