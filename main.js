11ef20e573901b0c91e6d1901177a865bd5a6b89
luisAngel <hluisangel96@gmail.com>
2026-07-23 20:05:19 -0600

    avances

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const hamburguerMenu=document.querySelector('#hamburguer-menu');
+const closeBtn=document.querySelector('#close-btn')
 const navbar=document.getElementById('navbar');
+const featuresBtn=document.querySelector('#features-btn')
+
 hamburguerMenu.addEventListener('click',()=>{
     navbar.style.display ='block'
 })
+
+closeBtn.addEventListener('click',()=>{
+    navbar.style.display ='none'
+})
+
+featuresBtn.addEventListener('click',()=>{
+    console.log('click');
+    
+})
+
