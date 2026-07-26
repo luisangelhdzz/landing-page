d3c2c2eab1c08f8487fcbce64da8efd2f7a4803c
luisAngel <hluisangel96@gmail.com>
2026-07-25 23:41:46 -0600

    avances hoy

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const hamburguerMenu=document.querySelector('#hamburguer-menu');
 const closeBtn=document.querySelector('#close-btn')
 const navbar=document.getElementById('navbar');
 const featuresBtn=document.querySelector('#features-btn')
-const featuresUl=DocumentType.querySelector('#features-ul')
+const companyBtn=document.querySelector('#company-btn')
+const featuresUl=document.querySelector('#features-ul')
+const companyUl=document.querySelector('#company-ul')
+
+
 hamburguerMenu.addEventListener('click',()=>{
     navbar.style.display ='block'
 })
@@ -12,7 +16,19 @@ closeBtn.addEventListener('click',()=>{
 })
 
 featuresBtn.addEventListener('click',()=>{
-    featuresUl.style.display
-    
+    if(featuresUl.style.display==='block'){
+        featuresUl.style.display='none'
+    }else{
+        featuresUl.style.display='block'
+    }
+})
+
+companyBtn.addEventListener('click',()=>{
+    if(companyUl.style.display==='block'){
+        companyUl.style.display='none'
+    }else{
+        companyUl.style.display='block'
+    }
+
 })
 
