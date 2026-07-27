95447a8ade4d3127a6657d34dc5e95fdad6d365b
luisAngel <hluisangel96@gmail.com>
2026-07-26 23:23:32 -0600

    terminada

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const hamburguerMenu=document.querySelector('#hamburguer-menu');
-const closeBtn=document.querySelector('#close-btn')
-const navbar=document.getElementById('navbar');
+const closeBtn=document.querySelector('#close-btn');
+const navbar=document.getElementById('navbar')
 const featuresBtn=document.querySelector('#features-btn')
 const companyBtn=document.querySelector('#company-btn')
 const featuresUl=document.querySelector('#features-ul')
 const companyUl=document.querySelector('#company-ul')
-
-
+const featuresBtnArrow=document.querySelector('#features-btn-arrow')
+const companyBtnArrow=document.querySelector('#company-btn-arrow')
 hamburguerMenu.addEventListener('click',()=>{
     navbar.style.display ='block'
 })
 
 closeBtn.addEventListener('click',()=>{
+   const screenSize=window.innerWidth;
+   if(screenSize>=768){
+    navbar.style.display ='block'
+   }else{
     navbar.style.display ='none'
+   }
 })
 
 featuresBtn.addEventListener('click',()=>{
@@ -21,6 +26,8 @@ featuresBtn.addEventListener('click',()=>{
     }else{
         featuresUl.style.display='block'
     }
+    featuresBtnArrow.style.transition='transform 0.3s ease'
+   featuresBtnArrow.style.transform = (featuresBtnArrow.style.transform==='rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
 })
 
 companyBtn.addEventListener('click',()=>{
@@ -29,6 +36,9 @@ companyBtn.addEventListener('click',()=>{
     }else{
         companyUl.style.display='block'
     }
+
+        companyBtnArrow.style.transition='transform 0.3s ease'
+   companyBtnArrow.style.transform = (companyBtnArrow.style.transform==='rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
 
 })
 
